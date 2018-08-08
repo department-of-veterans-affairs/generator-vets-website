262c84ab941f332dbc12f98674e93a71e4706d87
Erik P. Hansen <eph@erikphansen.com>
2018-08-08 15:30:38 -0700

    [wip] Update Yeoman generator to create a working form app (#4)
    * Update Yeoman generator to create a working form app
    
    Also add default answers to provide hints of expected values.
    Default value are "smart" as they depend on the answers to previous prompts.
    
    * Make the Yeoman script a little smarter and more helpful
    
    * Allow for choosing from three different starting form templates.
    
    Flesh out the blank and simple templates.
    
    * Make the prompts smarter
    
    * Enforce dangling commas
    
    * Fix some examples/defaults
    
    * Remove unused package

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,57 +7,87 @@ module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(`Welcome to the ${chalk.red('vets-website')} generator!`));
+    this.log(
+      `A guide for using this Yeoman generator, including example answers for each prompt, can be found at:\n${chalk.cyan(
+        'https://github.com/department-of-veterans-affairs/vets-website/blob/master/docs/GeneratorOptions.md',
+      )}\n`,
+    );
+    this.log(
+      `'Tutorial - Creating Your First Form' can be found at:\n${chalk.cyan(
+        'https://department-of-veterans-affairs.github.io/va-digital-services-platform-docs/docs/vets-developer-docs/vets-website/forms/form-tutorial.html',
+      )}\n`,
+    );
 
     const prompts = [
       {
         type: 'input',
         name: 'appName',
         message:
-          "What's the name of your application? This will be the default page title."
+          "What's the name of your application? This will be the default page title. Examples: '21P-530 Burials benefits form' or 'GI Bill School Feedback Tool'",
+        default: 'A New Form',
       },
       {
         type: 'input',
         name: 'folderName',
         message:
-          'What folder in src/js should your app live in? This can be a subfolder.',
+          "What folder in `src/applications/` should your app live in? This can be a subfolder. Examples: 'burials' or 'edu-benefits/0993'",
         validate: folder => {
           if (!folder.includes(' ')) {
             return true;
           }
 
           return 'Folder names should not include spaces';
-        }
+        },
+        // Remove leading and trailing forward slashes
+        filter: val => {
+          if (val.startsWith('/')) {
+            val = val.substring(1);
+          }
+          if (val.endsWith('/')) {
+            val = val.substring(0, -1);
+          }
+          return val;
+        },
+        default: 'new-form',
       },
       {
         type: 'input',
         name: 'entryName',
-        message: "What should be the name of your app's entry bundle?",
+        message:
+          "What should be the name of your app's entry bundle? Examples: '0993-edu-benefits' or 'complaint-tool'",
         validate: name => {
           if (!name.includes(' ')) {
             return true;
           }
 
           return 'Bundle names should not include spaces';
-        }
+        },
+        default: answers => answers.folderName.split('/').pop(),
       },
       {
         type: 'input',
         name: 'rootUrl',
-        message: "What's the root url for this app?",
-        validate: url => {
-          if (url.startsWith('/') && !url.endsWith('/')) {
-            return true;
+        message:
+          "What's the root url for this app? Examples: '/gi-bill-comparison-tool' or '/education/opt-out-information-sharing/opt-out-form-0993'",
+        filter: val => {
+          // Add leading slash if needed
+          if (!val.startsWith('/')) {
+            val = `/${val}`;
           }
-
-          return 'Urls should start with a / and not end with one';
-        }
+          // Add `index` if a page name was not included
+          if (val.endsWith('/')) {
+            val = `${val}index`;
+          }
+          return val;
+        },
+        default: answers => `/${answers.folderName}`,
       },
       {
         type: 'confirm',
         name: 'isForm',
         message: 'Is this a form app?',
-        default: false
-      }
+        default: false,
+      },
     ];
 
     return this.prompt(prompts).then(props => {
@@ -78,56 +108,53 @@ module.exports = class extends Generator {
         folderName: this.props.folderName,
         appName: this.props.appName,
         entryName: this.props.entryName,
-        subFolder: this.props.subFolder
+        subFolder: this.props.subFolder,
       });
     }
   }
 
   writing() {
-    const appPath = `src/js/${this.props.folderName}`;
+    const rootPath = `src/applications/`;
+    const appPath = `${rootPath}${this.props.folderName}`;
 
     this.fs.copyTpl(
       this.templatePath('manifest.json.ejs'),
       this.destinationPath(`${appPath}/manifest.json`),
-      this.props
+      this.props,
     );
     this.fs.copyTpl(
       this.templatePath('index.md.ejs'),
       this.destinationPath(`content/pages${this.props.rootUrl}.md`),
-      this.props
+      this.props,
     );
     this.fs.copyTpl(
       this.templatePath('e2e.spec.js.ejs'),
-      this.destinationPath(
-        `src/js/${this.props.folderName}/tests/00.${this.props.entryName}.e2e.spec.js`
-      ),
-      this.props
+      this.destinationPath(`${appPath}/tests/00.${this.props.entryName}.e2e.spec.js`),
+      this.props,
     );
     this.fs.copyTpl(
       this.templatePath('app-entry.jsx.ejs'),
       this.destinationPath(`${appPath}/app-entry.jsx`),
-      this.props
+      this.props,
     );
 
     if (!this.props.isForm) {
       this.fs.copy(
         this.templatePath('entry.scss'),
-        this.destinationPath(
-          `src/js/${this.props.folderName}/sass/${this.props.entryName}.scss`
-        )
+        this.destinationPath(`${appPath}/sass/${this.props.entryName}.scss`),
       );
 
       this.fs.copy(
         this.templatePath('reducer.js'),
-        this.destinationPath(`${appPath}/reducers/index.js`)
+        this.destinationPath(`${appPath}/reducers/index.js`),
       );
       this.fs.copy(
         this.templatePath('App.jsx'),
-        this.destinationPath(`${appPath}/containers/App.jsx`)
+        this.destinationPath(`${appPath}/containers/App.jsx`),
       );
       this.fs.copy(
         this.templatePath('routes.jsx'),
-        this.destinationPath(`${appPath}/routes.jsx`)
+        this.destinationPath(`${appPath}/routes.jsx`),
       );
     }
   }
--- generators/form/index.js
+++ generators/form/index.js
@@ -1,40 +1,74 @@
 'use strict';
 const Generator = require('yeoman-generator');
 
+const TEMPLATE_TYPES = {
+  BLANK: 'BLANK',
+  SIMPLE: 'SIMPLE',
+  COMPLEX: 'COMPLEX',
+};
+
+/**
+ * Helper that returns the last day of next year
+ * of M/D/YYYY
+ * 12/31/2019
+ */
+function getDate() {
+  const now = new Date();
+  const year = now.getFullYear() + 1;
+  return `12/31/${year}`;
+}
+
 module.exports = class extends Generator {
   prompting() {
     const prompts = [
       {
         type: 'input',
         name: 'formNumber',
-        message: "What's your form number?"
+        message: "What's your form number? Examples: '22-0993' or '21P-530'",
       },
       {
         type: 'input',
         name: 'trackingPrefix',
-        message: "What's the Google Analytics event prefix you want to use?"
+        message:
+          "What's the Google Analytics event prefix that you want to use? Examples: 'burials-530-' or 'edu-0993-'",
+        default: `${this.options.entryName}-`,
       },
       {
         type: 'input',
         name: 'respondentBurden',
-        message: "What's the respondent burden of this form in minutes?"
+        message: "What's the respondent burden of this form in minutes?",
+        default: '30',
       },
       {
         type: 'input',
         name: 'ombNumber',
-        message: "What's the OMB control number for this form?"
+        message: "What's the OMB control number for this form? Example: '2900-0797'",
       },
       {
         type: 'input',
         name: 'expirationDate',
-        message: "What's the OMB expiration date for this form?"
+        message:
+          "What's the OMB expiration date (in M/D/YYYY format) for this form? Example: '1/31/2019'",
+        default: getDate,
       },
       {
         type: 'input',
         name: 'benefitDescription',
         message:
-          "What's the benefit description for this form? Something like 'health care benefits'"
-      }
+          "What's the benefit description for this form? Examples: 'education benefits' or 'disability claims increase'",
+        default: `benefits`,
+      },
+      {
+        type: 'list',
+        name: 'templateType',
+        message: 'Which form template would you like to start with?',
+        choices: [
+          `${TEMPLATE_TYPES.BLANK}: A form without any fields'`,
+          `${TEMPLATE_TYPES.SIMPLE}: A single-chapter form with a single field'`,
+          `${TEMPLATE_TYPES.COMPLEX}: A complex, multi-chapter form with multiple fields`,
+        ],
+        filter: choice => choice.split(':')[0],
+      },
     ];
 
     return this.prompt(prompts).then(props => {
@@ -43,43 +77,64 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    const appPath = `src/js/${this.props.folderName}`;
+    const appPath = `src/applications/${this.props.folderName}`;
 
     this.fs.copyTpl(
       this.templatePath('entry.scss.ejs'),
       this.destinationPath(`${appPath}/sass/${this.props.entryName}.scss`),
-      this.props
+      this.props,
     );
 
     this.fs.copyTpl(
       this.templatePath('reducer.js.ejs'),
       this.destinationPath(`${appPath}/reducers/index.js`),
-      this.props
+      this.props,
     );
     this.fs.copyTpl(
       this.templatePath('App.jsx.ejs'),
+      // TODO: change the name of App.jsx to something like a ClassCase version of `${this.props.entryName}App.jsx`?
       this.destinationPath(`${appPath}/containers/App.jsx`),
-      this.props
+      this.props,
     );
     this.fs.copyTpl(
       this.templatePath('routes.jsx.ejs'),
       this.destinationPath(`${appPath}/routes.jsx`),
-      this.props
-    );
-    this.fs.copyTpl(
-      this.templatePath('form.js.ejs'),
-      this.destinationPath(`${appPath}/config/form.js`),
-      this.props
+      this.props,
     );
     this.fs.copyTpl(
       this.templatePath('IntroductionPage.jsx.ejs'),
       this.destinationPath(`${appPath}/containers/IntroductionPage.jsx`),
-      this.props
+      this.props,
     );
     this.fs.copyTpl(
       this.templatePath('ConfirmationPage.jsx.ejs'),
       this.destinationPath(`${appPath}/containers/ConfirmationPage.jsx`),
-      this.props
+      this.props,
     );
+    switch (this.props.templateType) {
+      case TEMPLATE_TYPES.BLANK:
+        this.fs.copyTpl(
+          this.templatePath('formBlank.js.ejs'),
+          this.destinationPath(`${appPath}/config/form.js`),
+          this.props,
+        );
+        break;
+      case TEMPLATE_TYPES.SIMPLE:
+        this.fs.copyTpl(
+          this.templatePath('formSimple.js.ejs'),
+          this.destinationPath(`${appPath}/config/form.js`),
+          this.props,
+        );
+        break;
+      case TEMPLATE_TYPES.COMPLEX:
+        this.fs.copyTpl(
+          this.templatePath('formComplex.js.ejs'),
+          this.destinationPath(`${appPath}/config/form.js`),
+          this.props,
+        );
+        break;
+      default:
+        break;
+    }
   }
 };
