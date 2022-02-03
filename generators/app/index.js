d26b38aba2611a98219813296619a2ae823437c3
Cbonade <cbonade@DESKTOP-MI88P0N.localdomain>
2022-02-03 16:49:19 -0500

    Update version

--- generators/app/index.js
+++ generators/app/index.js
@@ -16,6 +16,13 @@ function hasAccessTo(location) {
   }
 }
 
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
@@ -121,7 +128,7 @@ module.exports = class extends Generator {
         name: 'productId',
         message:
           "Select a UUID for your application. This should not overlap with any other application IDs to avoid overlapping data. Check the product-directory in GitHub to verify yours is not in use.",
-        default: crypto.randomUUID(),
+        default: uuidv4(),
       },
     ];
 
@@ -216,6 +223,7 @@ module.exports = class extends Generator {
         appName: this.props.appName,
         entryName: this.props.entryName,
         rootUrl: this.props.rootUrl,
+        productId: this. props.productId,
         template: {
           vagovprod: false,
           layout: 'page-react.html',
